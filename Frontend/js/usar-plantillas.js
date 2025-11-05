f42ca72cfa01f957ce37eb19cd9f15b32664b523
Richard <ccus.richard.rodriguez@gmail.com>
2025-11-04 21:03:16 -0500

    Imprementacion del about HTML/CSS

---
+++ Frontend/js/usar-plantillas.js
@@ -0,0 +1,13 @@
+async function AgregarHTML(clase, url) {
+  const container = document.querySelector(clase);
+
+  const res = await fetch(url);
+
+  container.innerHTML = await res.text();
+
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  AgregarHTML('header.page-header', 'Reutilizables/header.html');
+  AgregarHTML('footer.page-footer', 'Reutilizables/footer.html');
+});
