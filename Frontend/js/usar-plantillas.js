d0dceeb9d7b23d4c3a59f4d12a9c9be742fb0a7d
Richard <ccus.richard.rodriguez@gmail.com>
2025-11-05 18:57:20 -0500

    Frontend: Barra opciones-usuario/reportes-usuario

--- Frontend/js/usar-plantillas.js
+++ Frontend/js/usar-plantillas.js
@@ -1,13 +1,11 @@
 async function AgregarHTML(clase, url) {
   const container = document.querySelector(clase);
-
   const res = await fetch(url);
-
   container.innerHTML = await res.text();
-
 }
 
 window.addEventListener('DOMContentLoaded', () => {
   AgregarHTML('header.page-header', 'Reutilizables/header.html');
   AgregarHTML('footer.page-footer', 'Reutilizables/footer.html');
+  AgregarHTML('aside.opciones-usuario', '../Reutilizables/aside-user.html');
 });
